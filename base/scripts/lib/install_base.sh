faefda9cc29f1076171f6ea0ab774e37f562450a
igor <igvol44@gmail.com>
2017-10-05 15:33:38 +0300

    Dependencies added

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -2,3 +2,7 @@
 set -e
 apt-get update -y
 apt-get install -y curl bzip2 build-essential g++ python git
+apt-get install pkg-config -y
+add-apt-repository ppa:chris-lea/libpgm
+apt-get update
+apt-get install libzmq-dev
